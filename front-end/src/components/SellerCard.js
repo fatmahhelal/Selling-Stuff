e4510762b1802fc712f650a1f1860edaaf961341
ghaidaakhalee <51974001+ghaidaakhalee@users.noreply.github.com>
2021-02-01 15:56:26 +0300

    delet by hind

--- front-end/src/components/SellerCard.js
+++ front-end/src/components/SellerCard.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class SellerCard extends Component {
+  //function delete item end point delete
+  //item id will be {this.props.item._id}
+  deleteItem =()=>{
+    
+    axios
+      .delete(`http://localhost:5000/itemDelete?id=${this.props.item._id}`)
+      .then((response) => {
+        console.log("RESPONSE: ", response);
+        console.log("DATA: ", response.data);
+      })
+      .catch((err) => {
+        console.log("ERR: ", err);
+      });
+  }
+
+  
   render() {
     return (
     
@@ -14,17 +32,18 @@ export default class SellerCard extends Component {
               {/* <p class="card-text">{this.props.SellerInfo.name}</p> */}
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group botCon">
+                <Link to="/editItem">
                     <button
                       type="button"
-                      class="btn btn-outline-success btnMore"
-                    >
+                      class="btn btn-outline-success btnMore">
                       Edit
                     </button>
+                    </Link>
                   <a href="">
                     <button
                       type="button"
                       class="btn btn-outline-success btnMore"
-                    >
+                    onClick={this.deleteItem}>
                       {" "}
                       Delete
                     </button>
--- front-end/src/components/SellerInfo.js
+++ front-end/src/components/SellerInfo.js
@@ -1,12 +1,45 @@
+// import React, { Component } from 'react'
+// import SellerCard from './SellerCard'
+
+
+
+// export default class SellerInfo extends Component {
+//     render() {
+//         const ItemCard = this.props.Item.map((Item, key) => {
+//           return (
+//             <SellerCard
+//               key={key}
+//               item={Item}
+//             />
+           
+//           );
+//         });
+//         return <div className="topHeader boot">{ItemCard}</div>;
+//       }
+//     }
+
 import React, { Component } from 'react'
 import SellerCard from './SellerCard'
+import axios from "axios";
 
 
 
 export default class SellerInfo extends Component {
+  deleteAll =()=>{
+    axios
+      .delete(`http://localhost:5000/deleteAllItem?userId=${this.props.sellerId}`)
+      .then((response) => {
+        console.log("RESPONSE: ", response);
+        console.log("DATA: ", response.data);
+      })
+      .catch((err) => {
+        console.log("ERR: ", err);
+      });
+  }
     render() {
         const ItemCard = this.props.Item.map((Item, key) => {
           return (
+            
             <SellerCard
               key={key}
               item={Item}
@@ -14,7 +47,11 @@ export default class SellerInfo extends Component {
            
           );
         });
-        return <div className="topHeader boot">{ItemCard}</div>;
+        return <div className="topHeader boot">
+          <button onClick={this.deleteAll}>Delete All</button>
+          {ItemCard}</div>;
       }
     }
+
+
 
